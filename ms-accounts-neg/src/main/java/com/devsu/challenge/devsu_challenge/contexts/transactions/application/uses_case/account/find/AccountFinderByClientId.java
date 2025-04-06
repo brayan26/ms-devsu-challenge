275085f5413b1207cbd342528a14e2936db929ad
bparra26 <brayanpp2697@gmail.com>
2025-04-06 17:56:36 -0500

    actualizo casos de uso para buscar cuentas

--- ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/application/uses_case/account/find/AccountFinderByClientId.java
+++ ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/application/uses_case/account/find/AccountFinderByClientId.java
@@ -1,4 +1,20 @@
 package com.devsu.challenge.devsu_challenge.contexts.transactions.application.uses_case.account.find;
 
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.clazz.Account;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.repositories.IAccountRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class AccountFinderByClientId {
+   private final IAccountRepository repository;
+
+   public AccountFinderByClientId(IAccountRepository repository) {
+      this.repository = repository;
+   }
+
+   public List<Account> run(String clientId) {
+      return this.repository.findAccountsByClientId(clientId);
+   }
 }
