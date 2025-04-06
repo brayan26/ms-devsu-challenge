85a3814a8ad64681f8d9da86f814c77704f27aa0
bparra26 <brayanpp2697@gmail.com>
2025-04-06 01:26:47 -0500

    Agrego capa de serviivio para cuenta y movimientos

--- ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/infrastructure/service/AccountService.java
+++ ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/infrastructure/service/AccountService.java
@@ -1,4 +1,46 @@
-package com.devsu.challenge.devsu_challenge.contexts.accounts.infrastructure.service;
+package com.devsu.challenge.devsu_challenge.contexts.transactions.infrastructure.service;
 
+import com.devsu.challenge.devsu_challenge.contexts.shared.infrastructure.exceptions.GenericBadRequestException;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.application.uses_case.account.create.AccountCreatorUseCase;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.application.uses_case.account.find.AccountFinderByIdUseCase;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.application.uses_case.account.update.AccountInactivateUseCase;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.application.uses_case.account.update.BalanceUpdaterUseCase;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.clazz.Account;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.error.TransactionError;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
+   private final AccountCreatorUseCase accountCreatorUseCase;
+   private final AccountFinderByIdUseCase accountFinderByIdUseCase;
+   private final AccountInactivateUseCase accountInactivateUseCase;
+   private final BalanceUpdaterUseCase balanceUpdaterUseCase;
+
+   public Account createAccount(Account account) {
+      account.setId(null);
+      account.setCreatedAt(LocalDateTime.now());
+      account.setStatus(true);
+      if (account.getOpeningBalance().compareTo(BigDecimal.ZERO) < 0) {
+         throw new GenericBadRequestException("<AccountService.createAccount> negative opening balance",
+               TransactionError.builder().invalidBalance());
+      }
+      return this.accountCreatorUseCase.run(account);
+   }
+
+   public Account getAccountById(String id) {
+      return this.accountFinderByIdUseCase.run(id);
+   }
+
+   public void updateBalance(String accountId, BigDecimal balance) {
+      this.balanceUpdaterUseCase.run(accountId, balance);
+   }
+
+   public void inactiveAccount(String accountId) {
+      this.accountInactivateUseCase.run(accountId);
+   }
 }
