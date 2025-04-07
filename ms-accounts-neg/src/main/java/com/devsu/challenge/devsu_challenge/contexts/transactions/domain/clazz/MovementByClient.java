1a1b30deb79dc5ea7806d5be8a9eba0394dbf2df
bparra26 <brayanpp2697@gmail.com>
2025-04-07 12:53:29 -0500

    agrego clase de dominio para mapear la vista del reporte

--- ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/domain/clazz/MovementByClient.java
+++ ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/domain/clazz/MovementByClient.java
@@ -1,4 +1,22 @@
 package com.devsu.challenge.devsu_challenge.contexts.transactions.domain.clazz;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MovementByClient {
+   private String movementId;
+   private String client;
+   private String clientDni;
+   private String accountNumber;
+   private String accountType;
+   private String movementType;
+   private BigDecimal value;
+   private BigDecimal balance;
+   private String movementDate;
 }
--- ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/domain/repositories/IMovementRepository.java
+++ ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/domain/repositories/IMovementRepository.java
@@ -1,11 +1,12 @@
 package com.devsu.challenge.devsu_challenge.contexts.transactions.domain.repositories;
 
 import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.clazz.Movement;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.clazz.MovementByClient;
 
 import java.util.List;
 
 public interface IMovementRepository {
    Movement create(Movement movement);
    List<Movement> findMovementsByAccount(String accountId);
-   List<Movement> findMovementsByDateRange(String startDate, String endDate, String clientId);
+   List<MovementByClient> findMovementsByDateRange(String startDate, String endDate, String clientId);
 }
