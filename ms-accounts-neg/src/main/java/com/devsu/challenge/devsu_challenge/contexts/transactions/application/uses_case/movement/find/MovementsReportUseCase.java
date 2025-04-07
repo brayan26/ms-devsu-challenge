002311ed16f123e81ce608e317b09a6fbf9564d1
bparra26 <brayanpp2697@gmail.com>
2025-04-07 12:52:53 -0500

    agrego caso de uso para reporte de movimientos

--- ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/application/uses_case/movement/find/MovementsReportUseCase.java
+++ ms-accounts-neg/src/main/java/com/devsu/challenge/devsu_challenge/contexts/transactions/application/uses_case/movement/find/MovementsReportUseCase.java
@@ -1,4 +1,20 @@
 package com.devsu.challenge.devsu_challenge.contexts.transactions.application.uses_case.movement.find;
 
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.clazz.MovementByClient;
+import com.devsu.challenge.devsu_challenge.contexts.transactions.domain.repositories.IMovementRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class MovementsReportUseCase {
+   private final IMovementRepository repository;
+
+   public MovementsReportUseCase(IMovementRepository repository) {
+      this.repository = repository;
+   }
+
+   public List<MovementByClient> run(String startDate, String endDate, String clientId){
+      return this.repository.findMovementsByDateRange(startDate, endDate, clientId);
+   }
 }
